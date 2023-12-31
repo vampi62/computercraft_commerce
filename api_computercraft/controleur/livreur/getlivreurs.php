7383b57dc02fb7e42d53dac3c8e8da79059e0e5e
vampi62 <vadidi62@gmail.com>
2023-12-31 17:21:32 +0100

    fix bug suite test pt2

--- api_computercraft/controleur/livreur/getlivreurs.php
+++ api_computercraft/controleur/livreur/getlivreurs.php
@@ -2,9 +2,6 @@
 require_once('class/checkdroits.class.php');
 require_once('class/livreurs.class.php');
 
-if (!Checkdroits::checkArgs($_GET,array('id_livreur' => false))) {
-    return array('status_code' => 400, 'message' => 'Il manque des parametres.');
-}
 $sessionUser = Checkdroits::checkMode($bddConnection,$_GET,array('apikey' => true,'user' => true));
 if (isset($sessionUser['status_code'])) { // si un code d'erreur est retourné par la fonction alors on retourne le code d'erreur
     return $sessionUser; // error
